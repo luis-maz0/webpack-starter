fdcc8cc58166d0c88c8c05fe585d45591c9ae092
luis mazo <luis.mzo@hotmail.com>
2022-11-04 01:22:09 -0300

    Cambio en saludo del componente.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@
 import { saludar } from"./js/component";
 import "./style.css";
 
-
-const nombre = "luis";
-saludar(nombre);
+saludar();
--- src/js/component.js
+++ src/js/component.js
@@ -2,15 +2,10 @@
 import "../css/componente.css"
 //import webpackLogo from "../assets/imgs/webpack-logo.png";
 
-export const saludar = (nombre)=> { 
+export const saludar = ()=> { 
     //Agregando h1
     console.log("CREANDO ETIQUETA H1..");
     const h1 = document.createElement("h1");
-    h1.innerText = `Hola ${nombre}`;
+    h1.innerText = `Bienvenidos`;
     document.body.append(h1);
-
-    //Agregando img
-    // const img = document.createElement("img");
-    // img.src = webpackLogo; 
-    // document.body.append(img);
 }; 
